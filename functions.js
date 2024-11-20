6d0fbb7a9eb5196a13466705d112ea3774a373e2
k-james1 <keijames.travis.5z@stu.hosei.ac.jp>
2024-11-20 16:42:14 +0900

    add
    added

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 // .chapterクラスを持つ要素をすべて取得
 const chapters = document.querySelectorAll('.chapter');
-
 // 取得した各要素に対してアニメーションを適用
 chapters.forEach((chapter) => {
   gsap.fromTo(
@@ -19,4 +18,26 @@ chapters.forEach((chapter) => {
       }
     }
   );
+});
+
+// コンテナ要素とスライド要素を取得
+const wrapper = document.querySelector(".js-wrapper");
+const slides = gsap.utils.toArray(".js-scroll");
+
+// コンテナの幅を取得
+const wrapperWidth = wrapper.offsetWidth;
+
+// 横スクロールアニメーションの設定
+gsap.to(slides, {
+  xPercent: -100 * (slides.length - 1), // -X軸方向に移動
+  ease: "none", // アニメーションのイージング(noneは定速)
+  scrollTrigger: {
+    trigger: wrapper, // アニメーション開始のトリガー要素
+    pin: true, // 要素を固定
+    scrub: 1, // スクロール量に合わせてアニメーション
+    start: "top top", // アニメーションが始まる位置
+    end: `+=${wrapperWidth}`, // アニメーションが終わる位置
+    anticipatePin: 1, // ピン留めアニメーションをスムーズに開始
+    invalidateOnRefresh: true, // ページの再読み込み時(リサイズ時)に値を再計算する
+  },
 });
